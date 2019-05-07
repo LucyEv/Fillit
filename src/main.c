494142631fa52845b5e7c589d3f5f8ecafe772b4
Ivan Kozlov <ikozlov@e1z2r1p7.42.us.org>
2019-05-06 18:50:48 -0700

    Added main

--- src/main.c
+++ src/main.c
@@ -3,18 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 15:01:08 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/06 15:03:55 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/06 17:53:51 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fillit.h"
+
 #include "ft_printf.h"
+
+#include <fcntl.h>
 
 int		main(int ac, char *av[])
 {
-	if (ac == 2)
-		ft_printf("Hello, my first arg is: %s\n", av[1]);
+	int			fd;
+	int			count;
+	t_piece		tetris[MAX_PIECES];
+
+	if (ac != 2)
+		error_handler(ERR_INVALID_ARG, USAGE);
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		error_handler(ERR_FILE_NOT_FOUND, av[1]);
+	count = read_pieces(fd, tetris);
+	if (count < 0)
+		error_handler(ERR_INVALID_FILE, av[1]);
 	return (0);
 }
